8359ca46ae10e2c42616cccc7b88f1f3c8ea641a
Crisano97 <97keb6@gmail.com>
2022-07-04 14:55:04 +0200

    url immagine inserita tramite input

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const app = new Vue(
         el: '#root',
         data: {
             message: "",
-            imageUrl: "https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj"
+            imageUrl: "",
+            imageUrlImput: "",
+        }, 
+        methods: {
+            updateImageUrl(){
+                this.imageUrl = this.imageUrlImput;
+            }
         }
     }
 )
