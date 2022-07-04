3767c5d82e146176bd3e84da4cfc8106b334535e
Crisano97 <97keb6@gmail.com>
2022-07-04 14:36:16 +0200

    Bonus

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-            message: ""
+            message: "",
+            imageUrl: "https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj"
         }
     }
 )
